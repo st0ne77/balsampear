55cc6b85e2b7e8a0380da6b4981d933ac9c09d25
xiangwangshi <951251531@qq.com>
2019-07-25 23:26:14 +0800

    test ffmpeg

--- Player/VideoPlayer/main.cpp
+++ Player/VideoPlayer/main.cpp
@@ -1,25 +1,8 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include <QDebug>
-
-extern "C"
-{
-#include "libavcodec/avcodec.h"
-#include <libavformat/avformat.h>
-#include "libswscale/swscale.h"
-#include "libavdevice/avdevice.h"
-}
-
 
 int main(int argc, char *argv[])
 {
-    //这里简单的输出一个版本号
-    qDebug() << "Hello FFmpeg!" << endl;
-    av_register_all();
-    unsigned version = avcodec_version();
-    qDebug() << "version is:" << version;
-
-
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
